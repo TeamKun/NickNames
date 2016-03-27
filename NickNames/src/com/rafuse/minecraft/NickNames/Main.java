a4452e4f9ccbb821e968ae39182cdc7219c95ade
Halo4356 <mrafuse1997@gmail.com>
2016-03-27 00:18:47 -0300

    Data is now saved for each player

--- NickNames/src/com/rafuse/minecraft/NickNames/Main.java
+++ NickNames/src/com/rafuse/minecraft/NickNames/Main.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Sierra6767 on 3/26/2016.
@@ -50,6 +52,59 @@ public class Main extends JavaPlugin
             else if(args.length==1)
             {
                 String newName = args[0].replace("&", "§")+"§f§r";
+
+                File userFile = new File(new File("").getAbsolutePath()
+                        +"/plugins/NickNames/"+player.getName()+".yml");
+                if(userFile.isFile())
+                {
+                    PrintWriter writer;
+                    try
+                    {
+                        writer = new PrintWriter(userFile);
+                    }
+                    catch(IOException e)
+                    {
+                        getLogger().warning(e+"");
+                        writer = null;
+                    }
+                    if(writer != null)
+                    {
+                        writer.println(newName);
+                    }
+                    writer.close();
+                }
+                else
+                {
+                    getLogger().info("No file information found for "+player
+                    .getName()+": Will create now.");
+                    try
+                    {
+                        userFile.createNewFile();
+                    }
+                    catch(IOException e)
+                    {
+                        getLogger().warning(e+"");
+                    }
+
+                    PrintWriter writer;
+                    try
+                    {
+                        writer = new PrintWriter(userFile);
+                    }
+                    catch(IOException e)
+                    {
+                        getLogger().warning(e + "");
+                        writer = null;
+                    }
+
+                    if(writer != null)
+                    {
+                        writer.println(newName);
+                    }
+
+                    writer.close();
+                }
+
                 try
                 {
                     player.setDisplayName(newName);
